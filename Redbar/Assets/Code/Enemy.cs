e92f69b6e874458a3d503490386be351f3135fa4
laura-96 <laurii_96@hotmail.es>
2018-05-12 22:02:33 +0200

    Enemy script added

---
+++ Redbar/Assets/Code/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyType
+{
+    DRUNK,
+    GROUP,
+    CAR,
+    SILHOUETTE,
+    NONE
+}
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] public bool reacts = false;
+    [SerializeField] public EnemyType type;
+    [SerializeField] public int reactionProbability;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void Init(EnemyType enemy, int reactionProb)
+    {
+        reactionProbability = reactionProb;
+
+        int reaction = Random.Range(0, 100);
+        if (reaction <= reactionProbability)
+        {
+            reacts = true;
+        }
+        
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
