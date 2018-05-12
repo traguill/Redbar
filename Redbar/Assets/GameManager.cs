50ac00527494db193ca907f184629f738f9888c2
laura-96 <laurii_96@hotmail.es>
2018-05-12 11:44:58 +0200

    Piece script and player movement

---
+++ Redbar/Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+
+    [SerializeField] public GameObject player;
+
+    public float cellSize = 0.0f;
+
+    public GameManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Use this for initialization
+    void Start () {
+        player.GetComponent<Player>().Init(GameEnums.PieceColor.RED, GameEnums.PieceShape.SQUARE);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Redbar/Assets/Piece.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour {
+
+    // Use this for initialization
+    private GameEnums.PieceColor color;
+    private GameEnums.PieceShape shape;
+
+    private SpriteRenderer shape_renderer;
+
+    public void Init(GameEnums.PieceColor piece_col, GameEnums.PieceShape piece_shape)
+    {
+        color = piece_col;
+        shape = piece_shape;
+
+        shape_renderer = GetComponent<SpriteRenderer>();
+        switch (color)
+        {
+            case GameEnums.PieceColor.BLUE:
+                shape_renderer.material.color = Color.blue;
+                break;
+
+            case GameEnums.PieceColor.RED:
+                shape_renderer.material.color = Color.red;
+                break;
+        }
+    }
+
+    void Start () {
+        
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
