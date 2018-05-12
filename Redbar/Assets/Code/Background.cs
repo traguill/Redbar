3956b7e9a721c9ed36c4512dc733f68a482b69e6
traguill <traguill1@gmail.com>
2018-05-12 17:02:18 +0200

    Player movement

---
+++ Redbar/Assets/Code/Background.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+
+    public MeshFilter go_floor;
+    public MeshFilter go_wall;
+
+    Mesh floor;
+    Mesh wall;
+
+
+	// Use this for initialization
+	void Start () {
+        floor = go_floor.mesh;
+        wall = go_wall.mesh;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
+
+        if(IsOutOfCamera(ref planes, floor) && IsOutOfCamera(ref planes, wall))
+        {
+
+        }
+        
+       
+
+	}
+
+    bool IsOutOfCamera(ref Plane[] planes, Mesh s)
+    {
+        return GeometryUtility.TestPlanesAABB(planes, s.bounds);
+    }
+
+}
---
+++ Redbar/Assets/Code/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+    bool stop = false;
+    bool running = false;
+
+    public float bckMoveSpeed = 1.0f;
+    public float bckMoveSpeedFast = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            running = true;
+        if (Input.GetKeyUp(KeyCode.Space))
+            running = false;
+        // Move Objects to the left
+        float step = Time.deltaTime * (!running ? bckMoveSpeed : bckMoveSpeedFast);
+        transform.position += new Vector3(step, 0.0f, 0.0f);
+	}
+}
