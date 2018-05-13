0128a2bd830440b00a8bdbf19d8d6f8d234df0c0
Jordi Nunes <jordinunes96@gmail.com>
2018-05-13 05:36:41 +0200

    UI Manager for fade in and fade out at the end of the game

--- Redbar/Assets/Code/Game_Manager.cs
+++ Redbar/Assets/Code/Game_Manager.cs
@@ -3,22 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Game_Manager : MonoBehaviour {
+public class Game_Manager : MonoBehaviour
+{
 
     public static Game_Manager g_GameManager;
 
     [SerializeField] public GameObject message;
     [SerializeField] public GameObject player;
+    [SerializeField] public GameObject ui;
 
     public bool isTutorialFinished = false;
     public bool gameOver = false;
+    private bool fade_initiating = false;
+
     void Awake () {
         g_GameManager = this;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        if(gameOver == true && fade_initiating == false)
+        {
+            fade_initiating = true;
+            ui.GetComponent<UIManager>().FadeIn();
+        }
 	}
 
     public Actions GetPlayerState()
---
+++ Redbar/Assets/Code/UIManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public CanvasGroup uiElement;
+
+    public string winText;
+    public string loseText;
+    public string creditsText;
+
+    public void FadeIn()
+    {
+        StartCoroutine(FadeCanvasGroup(uiElement, uiElement.alpha, 1));
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(FadeCanvasGroup(uiElement, uiElement.alpha, 0));
+    }
+
+    public IEnumerator FadeCanvasGroup(CanvasGroup cg, float start, float end, float lerpTime = 0.5f)
+    {
+        float timeStarting = Time.time;
+        float timeSinceStarted = Time.time - timeStarting;
+        float percentage = timeSinceStarted / lerpTime;
+
+        while (true)
+        {
+            timeSinceStarted = Time.time - timeStarting;
+            percentage = timeSinceStarted / lerpTime;
+
+            float currentValue = Mathf.Lerp(start, end, percentage);
+
+            cg.alpha = currentValue;
+
+            if (percentage >= 1) break;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        print("done");
+    }
+
+    public void changeText(string newText)
+    {
+        uiElement.GetComponentInChildren<Text>().text = newText;
+    }
+}
